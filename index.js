eaaf330647cac553666e89fa8c4933ef34364719
C Martin <cmart131@yahoo.com>
2023-01-22 16:59:47 -0800

    added code to lib files. realized that I need to generate the HTML file so, going back to delete the HTML in dist folder and add an HTML template to src file

---
+++ index.js
@@ -0,0 +1,90 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+
+
+//  questions for user input
+
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Add a description of your project.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions and examples for use. Include screenshots as needed.',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Do you have any contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'Add testing instructions for your project.',  
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Did you use any of the following licenses on your project?',
+      choices: [ 'Apache', 'MIT', 'Mozilla-Public', 'GNU-GPL v3', 'ISC', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+  ];
+
+  
+
+// function to write HTML file
+function writeToFile(fileName, data) {
+  fs.writeFile(`${fileName}.md`, generateMarkdown(data), "utf8", function (error) {
+      if (error) {
+          console.log(error);
+      } else {
+          console.log(`Congratulations! Your README has been saved to ${fileName}.md.`);
+      }
+  });
+}
+
+//  function to initialize app
+function init() {
+  inquirer
+      .prompt(questions)
+      .then((response) => {
+          writeToFile(response.title, response);
+      })
+      .catch((error) => {
+          console.log(error);
+      })
+}
+
+
+// Function call to initialize app
+init();
