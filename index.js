fb4f1ff009f43cbc10df41d6de3837c613b6fced
C Martin <cmart131@yahoo.com>
2023-02-05 13:25:23 -0800

    figured out the HTML portion. The manager card isn't working correctly though, I'm not sure what I'm doing wrong

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const addManager = () => {
           type: 'input',
           message: "What is the manager's office number",
           name: "officeNumber",
-          validate: (managerONumberInput) => { if (managerONumberInput) { return true } else { return 'Manager office number is needed.' } },
+          validate: (managerONumberInput) => { if (managerONumberInput) { return true } else { return 'Office number is needed.' } },
       },
   ]).then(({ name, id, email, officeNumber }) => {
       const newManager = new Manager(name, id, email, officeNumber)
@@ -61,21 +61,21 @@ const addEngineer = () => {
       },
       {
           type: 'input',
-          message: "What is the Engineer's employee ID?",
+          message: "What is the engineer's employee ID?",
           name: "id",
           validate: (engineerIdInput) => { if (engineerIdInput) { return true } else { return 'Engineer ID is needed.' } },
       },
       {
           type: 'input',
-          message: "What is the Engineer's email address?",
+          message: "What is the engineer's email address?",
           name: "email",
           validate: (engineerEmailInput) => { if (engineerEmailInput) { return true } else { return 'Engineer email is needed.' } },
       },
       {
           type: 'input',
-          message: "What is the Engineer's Github username?",
+          message: "What is the engineer's Github username?",
           name: "github",
-          validate: (engineerGithubInput) => { if (engineerGithubInput) { return true } else { return 'Engineer Github address is needed.' } },
+          validate: (engineerGithubInput) => { if (engineerGithubInput) { return true } else { return 'Github address is needed.' } },
       },
   ]).then(({ name, id, email, github }) => {
       const newEngineer = new Engineer(name, id, email, github)
@@ -95,21 +95,21 @@ const addIntern = () => {
       },
       {
           type: 'input',
-          message: "What is the Intern's employee ID?",
+          message: "What is the intern's employee ID?",
           name: "id",
           validate: (internIdInput) => { if (internIdInput) { return true } else { return 'Intern ID is needed.' } },
       },
       {
           type: 'input',
-          message: "What is the Intern's email address?",
+          message: "What is the intern's email address?",
           name: "email",
           validate: (internEmailInput) => { if (internEmailInput) { return true } else { return 'Intern email is needed.' } },
       },
       {
           type: 'input',
-          message: "What is the Intern's school",
+          message: "What is the intern's current school",
           name: "school",
-          validate: (internSchoolInput) => { if (internSchoolInput) { return true } else { return 'Intern office number is needed.' } },
+          validate: (internSchoolInput) => { if (internSchoolInput) { return true } else { return 'Current school is needed.' } },
       },
   ]).then(({ name, id, email, school }) => {
       const newIntern = new Intern(name, id, email, school)
@@ -142,154 +142,3 @@ const createTeam = () => {
 //function to initialize app
 addManager();
 
-// //prompt questions
-// const questions = [
-//   {
-//     type: 'list',
-//     name: 'role',
-//     message: 'Please select the role of the employee.',
-//     choices: ['Manager','Engineer','Intern']
-//   },
-// {
-//     type: 'input',
-//     name: 'name',
-//     message: 'What is the name of the employee? (Required)',
-//     validate: nameInput => {
-//         if (nameInput) 
-//         {
-//         return true;
-//         } else {
-//             console.log('Please enter the name of this employee.');
-//             return false;
-//                }
-//     }
-// },
-// {
-//     type: 'input',
-//     name: 'id',
-//     message: 'What is the ID number of the employee? (Required)',
-//     validate: idInput => {
-//         if (idInput)
-//         {
-//         return true;
-//         } else {
-//             console.log('Please enter the employee ID.');
-//             return false;
-//                }
-//     }
-// },
-// {
-//     type: 'input',
-//     name: 'email',
-//     message: 'What is the employees email? (Required)',
-//     validate: emailInput => {
-//         if (emailInput.includes('@') ){
-//             return true;
-//           } else {
-//             console.log('Please enter the employee email address.');
-//             return false;
-//                }
-//     }
-// },
-// {
-//     type: 'input',
-//     name: 'officeNumber',
-//     message: 'Please enter the office number of the manager.',
-//     when:(officeNumberInput) => officeNumberInput.role ==='Manager', 
-//     validate: officeNumberInput => {
-//       if (officeNumberInput) {
-//         return true;
-//       } else {
-//         console.log('Please enter the office number of the manager.');
-//         return false;
-//       }
-//     }
-//   },
-//   {
-//     type: 'input',
-//     name: 'github',
-//     message: 'Please enter the GitHub username for the engineer.',
-//     when:(githubInput) => githubInput.role ==='Engineer', 
-//     validate: githubInput => {
-//       if (githubInput) {
-//         return true;
-//       } else {
-//         console.log('Please enter gitHub username.');
-//         return false;
-//       }
-//     }
-//   },
-//   {
-//     type: 'input',
-//     name: 'school',
-//     message: 'Please enter the name of current school.',
-//     when:(schoolInput) => schoolInput.role ==='Intern', 
-//     validate: schoolInput => {
-//       if (schoolInput) {
-//         return true;
-//       } else {
-//         console.log('Please enter the school name.');
-//         return false;
-//       }
-//     }
-//   }
-// ];
-
-// //prompt questions to add more team members
-// const addMember = () => {
-//   inquirer
-//     .prompt([
-//       {
-//         type: "list",
-//       name: "add",
-//       message: "Would you like to add a new employee?",
-//       choices: ["Yes", "No"]
-//       }
-//     ])
-//     .then(function(res) {
-//       if (res.add === "Yes") {
-//         inquirer
-//       .prompt(questions)
-
-//       } else {
-//         console.log("Done");
-//         completedTeam(teamList);
-//       }
-//     });
-// };
-
-// //creates the HTML file
-// function writeFile(fileName, data) {
-//   fs.writeFile(`${fileName}.html`, generateHTML(data), "utf8", function (error) {
-//       if (error) {
-//           console.log(error);
-//     }
-//     else{
-//       console.log('Congratulations! Your HTML file has been saved in the dist folder!');
-//     }
-//   });
-// };
-
-
-// // function to initialize app
-// function init() {
-//   inquirer
-//       .prompt(questions)
-//       .then((data) => {
-//           teamList.push(new manager(data.name, data.id, data.email, data.office));
-//           console.log(teamList); //
-//           addMember();
-//       })
-//       .then(teamList => {
-//         return generateHTML(teamList);
-//       })
-//       .then(HTMLPage => {
-//         return writeFile(HTMLPage);
-//       })
-//       .catch(err => {
-//      console.log(err);
-//       });
-// }
-
-// // function call to initialize app
-// init();
--- src/templateHTML.js
+++ src/templateHTML.js
@@ -1,38 +1,38 @@
 //create the team cards
-const generateTeam = team => {
+const generateTeam = (team) => {
 
   // create manager card
   const generateManager = manager => {
       return `
-      <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="card mx-auto mb-3" style="width: 18rem">
         <div class="card h-100">
             <div class="card-header">
-              <h3>${manager.getName()}</h3>
-              <h4>Manager</h4><i class="fas fa-mug-hot mr-2"></i>
+              <h3>${manager.name}</h3>
+              <h4>Manager</h4><i class="fas fa-mug-hot "></i>
             </div>
             <div class="card-body">
-              <p class="id">ID: ${manager.getId()}</p>
-              <p class="email"> Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
-              <p class="office"> Office Number: ${manager.getOfficeNumber()}</p>
+              <p class="id">ID: ${manager.id}</p>
+              <p class="email"> Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+              <p class="office"> Office Number: ${manager.officeNumber}</p>
             </div>
         </div>
       </div>
       `;
-  };
+  }
 
   //create engineer card
   const generateEngineer = engineer => {
     return `
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="card mx-auto mb-3" style="width: 18rem">
       <div class="card h-100">
           <div class="card-header">
-            <h3>${engineer.getName()}</h3>
+            <h3>${engineer.name}</h3>
             <h4>Engineer</h4><i class="fas fa-glasses mr-2"></i>
           </div>
           <div class="card-body">
-            <p class="id">ID: ${engineer.getId()}</p>
-            <p class="email"> Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
-            <p class="github"> GitHub: ${engineer.getGithub()}</p>
+            <p class="id">ID: ${engineer.id}</p>
+            <p class="email"> Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+            <p class="github"> GitHub: ${engineer.github}</p>
           </div>
       </div>
     </div>
@@ -42,65 +42,80 @@ const generateTeam = team => {
   //create intern card
   const generateIntern = intern => {
     return `
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="card mx-auto mb-3" style="width: 18rem">
       <div class="card h-100">
           <div class="card-header">
-            <h3>${intern.getName()}</h3>
+            <h3>${intern.name}</h3>
             <h4>Intern</h4><i class="fas fa-user-graduate mr-2"></i>
           </div>
           <div class="card-body">
-            <p class="id">ID: ${intern.getId()}</p>
-            <p class="email"> Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
-            <p class="school"> School: ${intern.getSchool()}</p>
+            <p class="id">ID: ${intern.id}</p>
+            <p class="email"> Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+            <p class="school"> School: ${intern.school}</p>
           </div>
       </div>
     </div>
     `;
   
   }
-  const html = [];
 
-  html.push(team
+    // Create an empty array for the html cards
+    const html = [];
+
+    // Add the manager to the empty array 
+    html.push(team
       .filter(employee => employee.getRole() === "Manager")
       .map(manager => generateManager(manager))
-  );
-  html.push(team
+    );
+
+    // Add the engineers to the array 
+    html.push(team
       .filter(employee => employee.getRole() === "Engineer")
       .map(engineer => generateEngineer(engineer))
       .join("")
-  );
-  html.push(team
+    );
+
+    // Add the interns to the array
+    html.push(team
       .filter(employee => employee.getRole() === "Intern")
       .map(intern => generateIntern(intern))
       .join("")
-  );
+    );
 
-  return html.join("");
-}
+    // Return the Completed HTML
+    return html.join("");
+  }
+  
 
-module.exports = team => {
+  // create HTML file
 
+  module.exports = team => {
+  
   return `
   <!DOCTYPE html>
-<html lang="en">
-<head>
+  <html lang="en">
+  <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
   <script src="https://kit.fontawesome.com/c502137733.js"></script>
   <title>Team Profile Generator</title>
-</head>
-<body>
-  <div class="banner">
+  </head>
+
+  <body>
+    <div class="banner">
       <h1>My Team</h1>
-  </div>
-  <div class="card-section1">
-      ${generateTeam(team)}
-  </div>
-</body>
-</html>`;
-};
+    </div>
+
+    <div class="card-section1">
+    ${generateTeam(team)}
+    </div>
+  </body>
+  
+  </html>`;
+  };
+
 
